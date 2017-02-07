3337a8de64ea6aae574f336d4a4d8ba464780ddb
Jacob Martin <jake92@WIN2.CS.KSU.EDU>
2017-02-07 14:24:02 -0600

    Added title and image input

--- server.js
+++ server.js
@@ -7,9 +7,11 @@
  */
  
  var http = require('http');
+ var url = require('url');
  var fs = require('fs');
  var port = 3040;
  
+ var config = JSON.parse(fs.readFileSync('config.json'));
  var stylesheet = fs.readFileSync('gallery.css');
  
  var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
@@ -30,12 +32,20 @@
  function buildGallery(imageTags){
 		   var html = '<!doctype html>';
 		       html += '<head>';
-			   html +=   '<title>Gallery</title>';
+			   html +=   '<title>' + config.title + '</title>';
 			   html +=   '<link href="gallery.css" rel="stylesheet" type="text/css">';
 			   html += '</head>';
 			   html += '<body>';
-			   html += ' <h1>Gallery</h1>';
+			   html += ' <h1>' + config.title + '</h1>';
+			   html += ' <form action="">';
+			   html += '   <input type="text" name="title">';
+			   html += '   <input type="submit" value="Change Gallery Title">';
+			   html += ' </form>';
 			   html += imageNamesToTags(imageTags).join(' ');
+			   html += '<form action="" method="POST" enctype="multipart/form-data">';
+			   html += ' <input type="file" name="image">';
+			   html += ' <input type="submit" value="Upload Image">';
+			   html += '</form>';
 			   html += ' <h1>Hello.</h1> Time is ' + Date.now();
 			   html += '</body>';
 		   return html;
@@ -68,11 +78,49 @@
  });	 
  }
  
- var server = http.createServer(function(req, res) {	 
-	 switch(req.url) {
+ function uploadImage(req, res){
+	 var body='';
+	 req.on('error', function(){
+		res.statusCode = 500;
+		res.end();
+	 });
+	 req.on('data', function(){
+		 body += data;
+	 });
+	 req.on('end', function(){
+		fs.writeFile('filename', body, function(){
+			if(err){
+				console.error(err);
+				res.statusCode = 500;
+				res.end();
+				return;
+			}
+			serveGallery(req, res);
+		}); 
+	 });
+ }
+ 
+ var server = http.createServer(function(req, res) {
+     //at most, the url should have two parts -
+	 //a resource and a querystring separated by a ?
+	 var urlParts = url.parse(req.url);
+	 
+	 if(urlParts.query){
+		 var matches = /title=(.+)($|&)/.exec(urlParts.query);
+		 if(matches && matches[1]){
+			 config.title = decodeURIComponent(matches[1]);
+			 fs.writeFile('config.json', JSON.stringify(config));
+		 }
+	 }
+	 
+	 switch(urlParts.pathname) {
 		 case '/':
 		 case '/gallery':
-		   serveGallery(req, res);
+		   if(req.method == 'GET'){ 
+		       serveGallery(req, res);
+		   }else if(req.method == 'POST'){
+			   uploadImage(req, res);
+		   }
 		   break;
 		 case '/gallery.css':
 		   res.setHeader('Content-Type', 'text/css');
